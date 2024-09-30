e8c1e24084b15547c5bdb5d3a2d46e31b19eecdf
DDbrother <66873499+DDbrother9999@users.noreply.github.com>
2024-09-30 18:51:53 -0400

    initial setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "MainTeleOp")
+public class TeleOp extends LinearOpMode {
+
+
+
+}
