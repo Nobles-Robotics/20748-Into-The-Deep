2dc50a47e9d327a31a5b028b9a35b586b992cfac
DDbrother <66873499+DDbrother9999@users.noreply.github.com>
2024-09-30 19:55:14 -0400

    fix: upstream sync

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 
-import com.acmerobotics.dashboard.config.Config;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "MainTeleOp")
-public class TeleOp extends LinearOpMode {
+public class MainTeleOp extends LinearOpMode {
 
-
+    public void runOpMode() {
+    }
 
 }
