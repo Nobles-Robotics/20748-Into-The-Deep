357ee861f40f167c75f11f0bcf5ebffc20fca13c
DDbrother <derekyin42009@gmail.com>
2024-12-06 09:29:25 -0500

    feat: drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -1,15 +1,57 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-
-
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.teleop.bot.Bot;
 
 @TeleOp(name = "MainTeleOp")
 public class MainTeleOp extends LinearOpMode {
 
+    private Bot bot;
+    private double driveSpeed = 1, driveMultiplier = 1;
+    private GamepadEx gp1, gp2;
+    private boolean fieldCentric;
+    @Override
     public void runOpMode() {
-        //bogus change to test commit
+
+        Servo myServo = hardwareMap.get(Servo.class, "servo0");
+
+        Bot.instance = null;
+        bot = Bot.getInstance(this);
+        gp1 = new GamepadEx(gamepad1);
+        bot.initializeMotors();
+        bot.stopMotors();
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            drive();
+        }
+    }
+
+    private void drive() { // Robot centric, drive multiplier default 1
+        driveSpeed = driveMultiplier - 0.5 * gp1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        driveSpeed = Math.max(0, driveSpeed);
+
+        if (fieldCentric) {
+            Vector2d driveVector = new Vector2d(-gp1.getLeftX(), -gp1.getLeftY()),
+                    turnVector = new Vector2d(-gp1.getRightX(), 0);
+            bot.driveFieldCentric(driveVector.getX() * driveSpeed,
+                    driveVector.getY() * driveSpeed,
+                    turnVector.getX() * driveSpeed
+            );
+        } else {
+            Vector2d driveVector = new Vector2d(gp1.getLeftX(), -gp1.getLeftY()),
+                    turnVector = new Vector2d(gp1.getRightX(), 0);
+            bot.driveRobotCentric(driveVector.getX() * driveSpeed,
+                    driveVector.getY() * driveSpeed,
+                    turnVector.getX() * driveSpeed
+            );
+        }
     }
 
 }
