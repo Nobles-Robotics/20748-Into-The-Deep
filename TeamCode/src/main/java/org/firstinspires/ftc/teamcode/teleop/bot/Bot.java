03b2e80a1b534c5ea18a6d9c9c95d5d9ac47a0c3
DDbrother <derekyin42009@gmail.com>
2025-01-24 14:34:54 -0500

    update: command groups

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/bot/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/bot/Bot.java
@@ -1,13 +1,21 @@
 
 package org.firstinspires.ftc.teamcode.teleop.bot;
 
+import dev.frozenmilk.mercurial.Mercurial;
 import dev.frozenmilk.mercurial.commands.Command;
 import dev.frozenmilk.mercurial.commands.groups.Parallel;
 import dev.frozenmilk.mercurial.commands.groups.Race;
 import dev.frozenmilk.mercurial.commands.groups.Sequential;
+import org.firstinspires.ftc.teamcode.util.BulkReads;
+import org.firstinspires.ftc.teamcode.util.SilkRoad;
 
+@Mercurial.Attach
+@Drive.Attach
 @Slides.Attach
+@BulkReads.Attach
 @Gripper.Attach
+@Arm.Attach
+@SilkRoad.Attach
 public class Bot{
     public static Command attachSpecimen(){
         return new Sequential(
