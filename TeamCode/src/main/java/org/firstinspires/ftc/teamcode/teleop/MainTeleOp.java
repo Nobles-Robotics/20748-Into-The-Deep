0c0f53b2d69637d41d09d350abe8a30bf3b9e791
DDbrother <derekyin42009@gmail.com>
2025-02-19 08:26:02 -0500

    fix: MainTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import dev.frozenmilk.mercurial.Mercurial;
 import dev.frozenmilk.mercurial.bindings.BoundGamepad;
+import dev.frozenmilk.mercurial.commands.groups.Parallel;
+import dev.frozenmilk.mercurial.commands.groups.Sequential;
 import org.firstinspires.ftc.teamcode.teleop.bot.*;
 import org.firstinspires.ftc.teamcode.util.Features.BulkReads;
 
@@ -23,35 +25,87 @@ public class MainTeleOp extends OpMode {
         BoundGamepad alex = Mercurial.gamepad1();
         BoundGamepad jeff = Mercurial.gamepad2();
 
-        alex.back().or(jeff.back()).onTrue(Bot.setState(Bot.State.HOME));
+
 
         alex.rightBumper().onTrue(Bot.setState(Bot.State.INTAKE_SAM));
         alex.leftBumper().onTrue(Bot.setState(Bot.State.OUTTAKE_SAM));
 
-        jeff.rightBumper().onTrue(Bot.setState(Bot.State.INTAKE_SPEC));
-        jeff.leftBumper().onTrue(Bot.setState(Bot.State.HOME));
+//        jeff.rightBumper().onTrue(Bot.setState(Bot.State.INTAKE_SPEC));
+//        jeff.leftBumper().onTrue(Bot.setState(Bot.State.HOME));
 
-        alex.leftStickButton().and(alex.leftStickY().conditionalBindState().greaterThanEqualTo(0.5).bind()).onTrue(
-                Arm.setPower(0.75)
+        alex.leftStickButton().and(alex.leftStickY().conditionalBindState().greaterThanEqualTo(0.5).bind()).whileTrue(
+                Arm.setPower(0.5)
+        );
+        alex.leftStickButton().and(alex.leftStickY().conditionalBindState().lessThanEqualTo(-0.5).bind()).whileFalse(
+                Arm.setPower(-0.5)
         );
 
         alex.rightTrigger().conditionalBindState().greaterThanEqualTo(0.5).bind().onTrue(
-                Arm.setPower(0.75)
+                Drive.slow()
         );
         alex.leftTrigger().conditionalBindState().greaterThanEqualTo(0.5).bind().onTrue(
-                Arm.setPower(-0.75)
+                Drive.fast()
         );
+
         alex.a().or(jeff.a()).onTrue(
-                Bot.setState(Bot.State.OUTTAKE_HIGH)
+                //Bot.setState(Bot.State.INTAKE_SPEC)
+                new Parallel(
+                        Slides.runToPosition(Slides.wall),
+                        Gripper.close()
+                )
         );
+
         alex.b().or(jeff.b()).onTrue(
-                Bot.setState(Bot.State.SCORE_HIGH)
+                new Parallel(
+                        Slides.runToPosition(Slides.wall),
+                        Gripper.open()
+                )
         );
+
         alex.x().or(jeff.x()).onTrue(
-                Bot.setState(Bot.State.OUTTAKE_LOW)
+                new Parallel(
+                        Gripper.close(),
+                        Slides.runToPosition(Slides.highPos)
+                )
         );
+
         alex.y().or(jeff.y()).onTrue(
-                Bot.setState(Bot.State.SCORE_LOW)
+                new Sequential(
+                        Slides.runToPosition(Slides.scoreHighPos),
+                        Gripper.open()
+                )
+        );
+
+
+//        alex.x().or(jeff.x()).onTrue(
+//                Bot.setState(Bot.State.OUTTAKE_LOW)
+//        );
+//        alex.y().or(jeff.y()).onTrue(
+//                Bot.setState(Bot.State.SCORE_LOW)
+//        );
+
+
+        jeff.leftStickY().conditionalBindState().greaterThanEqualTo(0.5).bind().whileTrue(
+                Slides.setPowerUp(1)
+        );
+        jeff.leftStickY().conditionalBindState().lessThanEqualTo(-0.5).bind().whileTrue(
+                Slides.setPowerUp(-1)
+        );
+        jeff.leftStickY().conditionalBindState().lessThan(0.5).greaterThan(-0.5).bind().whileTrue(
+                Slides.setPowerUp(0)
+        );
+        jeff.rightStickY().conditionalBindState().greaterThanEqualTo(0.5).bind().whileTrue(
+                Slides.setPowerDown(1)
+        );
+        jeff.rightStickY().conditionalBindState().lessThanEqualTo(-0.5).bind().whileTrue(
+                Slides.setPowerDown(-1)
+        );
+        jeff.rightStickY().conditionalBindState().lessThan(0.5).greaterThan(-0.5).bind().whileTrue(
+                Slides.setPowerDown(0)
+        );
+
+        jeff.dpadDown().onTrue(
+                Slides.resetCommand()
         );
     }
 
