cdbaf019ad7d21b226326335971c6059750c35cc
DDbrother <derekyin42009@gmail.com>
2025-02-19 08:26:36 -0500

    fix: hwMap Names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Names.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Names.java
@@ -1,17 +1,21 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+
 public class Names {
 
-    public static final String vertSlideUp = "motor0";
-    public static final String vertSlideDown = "motor1";
-    public static final String arm = "motor1";
-    public static final String gripper = "servo0";
-    public static final String spinTake = "servo4";
-    public static final String wrist = "servo2";
-    public static final String frontLeft = "motorExp0";
-    public static final String frontRight = "motorExp1";
-    public static final String backLeft = "motorExp2";
-    public static final String backRight = "motorExp3";
+    public static final String vertSlideUp = "motorExp0";
+    public static final String vertSlideDown = "motorExp1";
+    public static final String arm = "motorExp2";
+    public static final String gripper = "servo2";
+    public static final String spinTake = "servoExp0";
+    public static final String wrist = "servo0";
+    public static final String frontLeft = "motor3";
+    public static final String frontRight = "motor1";
+    public static final String backLeft = "motor2";
+    public static final String backRight = "motor0";
     public static final String color = "color";
     public static final String touch = "touch";
 }
